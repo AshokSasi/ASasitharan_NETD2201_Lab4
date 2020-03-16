5d85ddc5d4ee08d821b6ba1fe1cddfacefcda2de
AshokSasi <60941595+AshokSasi@users.noreply.github.com>
2020-03-15 22:16:18 -0400

    Add files via upload

---
+++ ASasitharan_Lab4/CarInventory.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CarInventory
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblMake = New System.Windows.Forms.Label()
+        Me.lblYear = New System.Windows.Forms.Label()
+        Me.lblModel = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.cbMake = New System.Windows.Forms.ComboBox()
+        Me.lvwCars = New System.Windows.Forms.ListView()
+        Me.colNew = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colId = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colMake = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colModel = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colYears = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.colPrices = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.numericupdownYear = New System.Windows.Forms.NumericUpDown()
+        Me.txtModel = New System.Windows.Forms.TextBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.chkNew = New System.Windows.Forms.CheckBox()
+        Me.carInventoryToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        CType(Me.numericupdownYear, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblMake
+        '
+        Me.lblMake.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMake.Location = New System.Drawing.Point(37, 22)
+        Me.lblMake.Name = "lblMake"
+        Me.lblMake.Size = New System.Drawing.Size(41, 21)
+        Me.lblMake.TabIndex = 0
+        Me.lblMake.Text = "&Make:"
+        Me.lblMake.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblYear
+        '
+        Me.lblYear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblYear.Location = New System.Drawing.Point(37, 88)
+        Me.lblYear.Name = "lblYear"
+        Me.lblYear.Size = New System.Drawing.Size(35, 15)
+        Me.lblYear.TabIndex = 4
+        Me.lblYear.Text = "&Year:"
+        Me.lblYear.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblModel
+        '
+        Me.lblModel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblModel.Location = New System.Drawing.Point(37, 57)
+        Me.lblModel.Name = "lblModel"
+        Me.lblModel.Size = New System.Drawing.Size(45, 15)
+        Me.lblModel.TabIndex = 2
+        Me.lblModel.Text = "M&odel:"
+        Me.lblModel.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblPrice
+        '
+        Me.lblPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrice.Location = New System.Drawing.Point(37, 115)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(38, 15)
+        Me.lblPrice.TabIndex = 6
+        Me.lblPrice.Text = "&Price:"
+        Me.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'cbMake
+        '
+        Me.cbMake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbMake.FormattingEnabled = True
+        Me.cbMake.Items.AddRange(New Object() {"Acura", "Audi", "Bentley", "Buick", "Chevorlet", "Ford", "Honda", "Hyundai", "Jeep", "Toyota", "Volkswagen", "", "", "", ""})
+        Me.cbMake.Location = New System.Drawing.Point(89, 21)
+        Me.cbMake.Name = "cbMake"
+        Me.cbMake.Size = New System.Drawing.Size(121, 21)
+        Me.cbMake.TabIndex = 1
+        Me.carInventoryToolTip.SetToolTip(Me.cbMake, "Choose the car make.")
+        '
+        'lvwCars
+        '
+        Me.lvwCars.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.colNew, Me.colId, Me.colMake, Me.colModel, Me.colYears, Me.colPrices})
+        Me.lvwCars.FullRowSelect = True
+        Me.lvwCars.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable
+        Me.lvwCars.HideSelection = False
+        Me.lvwCars.Location = New System.Drawing.Point(38, 204)
+        Me.lvwCars.MultiSelect = False
+        Me.lvwCars.Name = "lvwCars"
+        Me.lvwCars.Size = New System.Drawing.Size(359, 186)
+        Me.lvwCars.TabIndex = 9
+        Me.carInventoryToolTip.SetToolTip(Me.lvwCars, "Select a car to edit.")
+        Me.lvwCars.UseCompatibleStateImageBehavior = False
+        Me.lvwCars.View = System.Windows.Forms.View.Details
+        '
+        'colNew
+        '
+        Me.colNew.Text = "New"
+        Me.colNew.Width = 47
+        '
+        'colId
+        '
+        Me.colId.Text = "ID"
+        Me.colId.Width = 46
+        '
+        'colMake
+        '
+        Me.colMake.Text = "Make"
+        Me.colMake.Width = 64
+        '
+        'colModel
+        '
+        Me.colModel.Text = "Model"
+        Me.colModel.Width = 65
+        '
+        'colYears
+        '
+        Me.colYears.Text = "Year"
+        Me.colYears.Width = 52
+        '
+        'colPrices
+        '
+        Me.colPrices.Text = "Price"
+        Me.colPrices.Width = 79
+        '
+        'numericupdownYear
+        '
+        Me.numericupdownYear.Location = New System.Drawing.Point(89, 88)
+        Me.numericupdownYear.Maximum = New Decimal(New Integer() {2020, 0, 0, 0})
+        Me.numericupdownYear.Minimum = New Decimal(New Integer() {1920, 0, 0, 0})
+        Me.numericupdownYear.Name = "numericupdownYear"
+        Me.numericupdownYear.Size = New System.Drawing.Size(120, 20)
+        Me.numericupdownYear.TabIndex = 5
+        Me.carInventoryToolTip.SetToolTip(Me.numericupdownYear, "Choose the year the car was manufactured in.")
+        Me.numericupdownYear.Value = New Decimal(New Integer() {1920, 0, 0, 0})
+        '
+        'txtModel
+        '
+        Me.txtModel.Location = New System.Drawing.Point(89, 56)
+        Me.txtModel.Multiline = True
+        Me.txtModel.Name = "txtModel"
+        Me.txtModel.Size = New System.Drawing.Size(120, 20)
+        Me.txtModel.TabIndex = 3
+        Me.carInventoryToolTip.SetToolTip(Me.txtModel, "Enter the car's model.")
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(89, 115)
+        Me.txtPrice.Multiline = True
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(120, 20)
+        Me.txtPrice.TabIndex = 7
+        Me.carInventoryToolTip.SetToolTip(Me.txtPrice, "Enter the price of the car.")
+        '
+        'lblOutput
+        '
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Location = New System.Drawing.Point(38, 393)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(359, 103)
+        Me.lblOutput.TabIndex = 10
+        Me.carInventoryToolTip.SetToolTip(Me.lblOutput, "Displays output messages.")
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(38, 517)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(83, 32)
+        Me.btnEnter.TabIndex = 11
+        Me.btnEnter.Text = "&Enter"
+        Me.carInventoryToolTip.SetToolTip(Me.btnEnter, "Press to enter a car's information.")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(174, 517)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(83, 32)
+        Me.btnReset.TabIndex = 12
+        Me.btnReset.Text = "&Reset"
+        Me.carInventoryToolTip.SetToolTip(Me.btnReset, "Press to reset the form.")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(314, 517)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(83, 32)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "&Exit"
+        Me.carInventoryToolTip.SetToolTip(Me.btnExit, "Press to close the application.")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'chkNew
+        '
+        Me.chkNew.CheckAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.chkNew.Location = New System.Drawing.Point(0, 141)
+        Me.chkNew.Name = "chkNew"
+        Me.chkNew.Size = New System.Drawing.Size(92, 24)
+        Me.chkNew.TabIndex = 8
+        Me.chkNew.Text = "&New"
+        Me.chkNew.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.carInventoryToolTip.SetToolTip(Me.chkNew, "Select if the car is new or not.")
+        Me.chkNew.UseVisualStyleBackColor = True
+        '
+        'CarInventory
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(428, 561)
+        Me.Controls.Add(Me.chkNew)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.txtModel)
+        Me.Controls.Add(Me.numericupdownYear)
+        Me.Controls.Add(Me.lvwCars)
+        Me.Controls.Add(Me.cbMake)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.lblModel)
+        Me.Controls.Add(Me.lblYear)
+        Me.Controls.Add(Me.lblMake)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "CarInventory"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Car Inventory"
+        CType(Me.numericupdownYear, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMake As Label
+    Friend WithEvents lblYear As Label
+    Friend WithEvents lblModel As Label
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents cbMake As ComboBox
+    Friend WithEvents lvwCars As ListView
+    Friend WithEvents colNew As ColumnHeader
+    Friend WithEvents colId As ColumnHeader
+    Friend WithEvents colMake As ColumnHeader
+    Friend WithEvents numericupdownYear As NumericUpDown
+    Friend WithEvents txtModel As TextBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents chkNew As CheckBox
+    Friend WithEvents colModel As ColumnHeader
+    Friend WithEvents colYears As ColumnHeader
+    Friend WithEvents colPrices As ColumnHeader
+    Friend WithEvents carInventoryToolTip As ToolTip
+End Class
---
+++ ASasitharan_Lab4/CarInventory.vb
@@ -0,0 +1,227 @@
+﻿'Name: Ashok Sasitharan
+'Student Id: 100745484
+'Date: March 13 2020
+'Description: This application will take in the details of a car such as the make,model,price,year,
+'And whether the car Is New Or Not And will add the car to a list. The added car's information can also be edited.
+Option Strict On
+Public Class CarInventory
+    'Global Variables
+    Dim selectedCar As Car
+    Dim isCarSelected As Boolean = False
+    Dim carList As New List(Of Car)
+    Dim isAddingToListView As Boolean = False
+
+    ''' <summary>
+    ''' When the form loads set the numeric up down's value to the max value
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'set the year to 2020 on load
+        numericupdownYear.Value = numericupdownYear.Maximum
+    End Sub
+    ''' <summary>
+    ''' This button closes the application when clicked
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    ''' <summary>
+    ''' Checks if form inputs are valid and then adds them to the listview in a new car object or edits previous data entries when pressed
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'calls IsValid and runs if user input is valid
+        If IsValid() = True Then
+            'run if a car is selected
+            If Not isCarSelected Then
+                'Create a new car object
+                selectedCar = New Car(cbMake.Text, txtModel.Text, Convert.ToInt32(numericupdownYear.Value), Convert.ToDecimal(txtPrice.Text), chkNew.Checked)
+                'add the car to the car list
+                carList.Add(selectedCar)
+                'outputs message with car data
+                lblOutput.Text = "It worked! " & vbCrLf & selectedCar.GetCarData
+                'runs if a car exitst in the list
+            ElseIf selectedCar.Id.Trim.Length > 0 Then
+                'set old data with new inputs
+                selectedCar.Make = cbMake.Text
+                selectedCar.Model = txtModel.Text
+                selectedCar.Price = Convert.ToDecimal(txtPrice.Text)
+                selectedCar.Year = Convert.ToInt32(numericupdownYear.Value)
+                selectedCar.IsNew = chkNew.Checked
+                'outputs message with car data
+                lblOutput.Text = "It worked! " & vbCrLf & selectedCar.GetCarData
+            End If
+            'call set defaults
+            SetDefaults()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' event handler that is used when a character in the list is selected
+    ''' </summary>
+    Private Sub lvwCars_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvwCars.SelectedIndexChanged
+        Dim priceInput As Decimal = 0.0D
+
+        'runs ifa list view item is selected
+        If lvwCars.SelectedIndices.Count = 1 Then
+            selectedCar = carList(lvwCars.SelectedIndices(0))
+            'set isCarSelected to true
+            isCarSelected = True
+            'set form inputs to the selected cars inputs
+            cbMake.Text = selectedCar.Make
+            txtModel.Text = selectedCar.Model
+            numericupdownYear.Value = selectedCar.Year
+            txtPrice.Text = selectedCar.Price.ToString
+            chkNew.Checked = selectedCar.IsNew
+
+        Else
+            'set isCarSelected to false
+            isCarSelected = False
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' This procedure stops an entry's checkbox from being checked in the listview
+    ''' </summary>
+    Private Sub lvwCars_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles lvwCars.ItemCheck
+
+        ' run if a car is not currently being added to the list
+        If Not isAddingToListView Then
+
+            ' keep the old checkbox value
+            e.NewValue = e.CurrentValue
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' This procedure sets the form controls to their defualt state and call the populate list procedure
+    ''' </summary>
+    Private Sub SetDefaults()
+        'sets the make combo box to blank
+        cbMake.SelectedIndex = -1
+        'set model textbox to blank
+        txtModel.Text = ""
+        'set price textbox to blank
+        txtPrice.Text = ""
+        'set the is New checkbox to unchecked
+        chkNew.Checked = False
+        'set the numeric up downs year to 2020 which is the maximum
+        numericupdownYear.Value = numericupdownYear.Maximum
+        'set focus to make combo box
+        Me.cbMake.Focus()
+        'call populate list
+        PopulateList()
+
+
+    End Sub
+
+    ''' <summary>
+    ''' This function checks the inputs and displays error messages if input is invalid
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function IsValid() As Boolean
+        'Variables
+        Dim errorMessage As String = String.Empty
+        Dim isValidInput As Boolean = True
+        Dim priceInput As Decimal = 0.0D
+
+        'run if make is not chosen
+        If cbMake.Text = String.Empty Then
+            'add error message
+            errorMessage += "Please select a make for the car." & vbCrLf
+
+            ' set isValidInput to false
+            isValidInput = False
+        End If
+
+        'run if model input is blank
+        If txtModel.Text = String.Empty Then
+
+            ' add error message
+            errorMessage += "Please enter the model of the car." & vbCrLf
+            txtModel.Focus()
+            ' set isValidInput to false
+            isValidInput = False
+
+        End If
+
+        'run if the price is empty or not numeric
+        If Not Decimal.TryParse(txtPrice.Text, priceInput) Then
+
+            ' add error message
+            errorMessage += "Price must be a number greater than 0." & vbCrLf
+            txtPrice.Focus()
+            txtPrice.SelectAll()
+
+            ' set isValidInput to false
+            isValidInput = False
+
+            'run if the price was less than 0
+        ElseIf Convert.ToDecimal(txtPrice.Text) < 0 Then
+            ' add error message
+            errorMessage += "Please enter a price greater than or equal to $0.00." & vbCrLf
+            txtPrice.Focus()
+            txtPrice.SelectAll()
+
+            ' set isValidInput to false
+            isValidInput = False
+        End If
+
+        If isValidInput = False Then
+            ' display the error message in output textbox
+            lblOutput.Text = errorMessage
+        End If
+        'return the boolean value of isValidInput
+        Return isValidInput
+    End Function
+
+    ''' <summary>
+    ''' This procedure populates the listview with the inputted car data
+    ''' </summary>
+    Sub PopulateList()
+
+        ' Clear the items from the listview control
+        lvwCars.Items.Clear()
+
+        ' This For loop re-populates the list
+        For index As Integer = 0 To carList.Count - 1
+            'create a new listview item
+            Dim carItem As New ListViewItem()
+            'makes checkboxes appear on the listview
+            lvwCars.CheckBoxes = True
+
+            'assign the values to the listview item
+            carItem.Checked = carList(index).IsNew
+            carItem.SubItems.Add(carList(index).Id.ToString())
+            carItem.SubItems.Add(carList(index).Make)
+            carItem.SubItems.Add(carList(index).Model)
+            carItem.SubItems.Add(carList(index).Year.ToString())
+            carItem.SubItems.Add(carList(index).Price.ToString("c"))
+
+            'set to true to show that a car is being inputted into the list
+            isAddingToListView = True
+
+            'add the car's details to the listview
+            lvwCars.Items.Add(carItem)
+
+            ' set isAddingToListView to false to show that a car is not being inputted to the list
+            isAddingToListView = False
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' This button sets the form to its default state when pressed
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ' call setDefaults procedure
+        SetDefaults()
+    End Sub
+End Class
